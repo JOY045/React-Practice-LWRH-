03eb4f0accb4fbe69d9c4bbb5a1bfb9c448456d7
JOY045 <shoeb.mahmud0099@gmail.com>
2024-05-21 12:37:15 +0600

    map in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 const App = () => {
 
-  let marks = 90;
+  const cities = ['Dhaka', 'Sylhet', 'Rangpur', 'Barisal', 'Khulna']
 
   return (
     <div>
-      {(() => {
-          if (marks >= 90) {
-            return "A+";
-          } else if (marks >= 80) {
-            return "A";
-          } else if (marks >= 70) {
-            return "B";
-          } else if (marks >= 60) {
-            return "C";
-          } else if (marks >= 50) {
-            return "D";
-          } else if (marks >= 40) {
-            return "E";
-          } else if (marks >= 30) {
-            return "F";
-          } else {
-            return "Fail";
-          }
-        })()}
+      <ul>
+        {
+          cities.map((city, index) => {
+            return <li key={index}>{city}</li>
+          })
+        }
+      </ul>
     </div>
   );
 };
