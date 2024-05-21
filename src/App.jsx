9aa4b6bc3f3e25b5ab8194fc4d83bc633ac2968a
JOY045 <shoeb.mahmud0099@gmail.com>
2024-05-21 12:58:38 +0600

    conditional rendering if else statement with function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-const App = () => {
-
-  const status = true;
-
+const loginStatusButton = (status) => {
   if (status == true) {
     return (
-      <div>
-        <h1>Logged In</h1>
-        <button>Logout</button>
-      </div>
+      <button>Logout</button>
     )
   }
-
   else {
     return (
-      <div>
-        <h1>Logged Out</h1>
-        <button>Login</button>
-      </div>
+      <button>Login</button>
     )
   }
+}
+
+
+const App = () => {
+
+  // if (status == true) {
+  //   return (
+  //     <div>
+  //       <h1>Logged In</h1>
+  //       <button>Logout</button>
+  //     </div>
+  //   )
+  // }
+
+  // else {
+  //   return (
+  //     <div>
+  //       <h1>Logged Out</h1>
+  //       <button>Login</button>
+  //     </div>
+  //   )
+  // }
+
+  return (
+    <div>
+      <h1>Login Status</h1>
+      {loginStatusButton(true)}
+    </div>
+  )
+
 };
 
 export default App;
