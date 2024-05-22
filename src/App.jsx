32f19f701daa08b722e3af05d58dcb9074cd6d25
JOY045 <shoeb.mahmud0099@gmail.com>
2024-05-22 17:35:22 +0600

    form submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 const App = () => {
+
+  const postFormData = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <div>
-      <button onClick={() => {
-        alert("You Clicked Me")
-      }}>Submit</button>
+      <form onSubmit={postFormData}>
+        <input type="text" placeholder="name" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
