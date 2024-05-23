442fc415d4f23e6ba0edf80133648262609cc5e3
JOY045 <shoeb.mahmud0099@gmail.com>
2024-05-23 18:26:02 +0600

    using useState in todo list

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,52 @@ import { useState } from "react";
 
 const App = () => {
 
-  const [myObj, setMyObj] = useState({
-    key1: "value 1",
-    key2: "value 2",
-    key3: "value 3"
-  });
+  const [list, setList] = useState([])
+  const [item, setItem] = useState("")
 
-  const change = () => {
-    setMyObj(
-      {
-       ...myObj,
-        key1: "new value 1"
-      }
-    )
+  const AddToList = () => {
+    list.push(item)
+    setList([...list])
+  }
+
+  const removeItem = (index) => {
+    list.splice(index, 1)
+    setList([...list])
   }
 
   return (
     <div>
-      <h1>{myObj.key1}</h1>
-      <button onClick={change}>Change</button>
+
+      <input onChange={(e) => setItem(e.target.value)} placeholder="Item" />
+      <button onClick={AddToList}>Add</button>
+
+      <table>
+        <tbody>
+          {
+            list.length !== 0 ? (
+              list.map((element, index) => {
+                return (
+                  <tr>
+                    <td>{element}</td>
+                    <td>
+                      <button
+                        onClick = {() => {
+                          removeItem(index)
+                        }}>
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            )
+              :
+              (<tr>
+                <td>No Items</td>
+              </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 };
