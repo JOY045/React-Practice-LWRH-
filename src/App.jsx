8dfcd734124b79df87e61af2cbdb9ac7251173a2
JOY045 <shoeb.mahmud0099@gmail.com>
2024-05-24 18:25:02 +0600

    useEffect using promise

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,19 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 const App = () => {
 
-  const [list, setList] = useState([])
-  const [item, setItem] = useState("")
+  const [data, setData] = useState([])
 
-  const AddToList = () => {
-    list.push(item)
-    setList([...list])
-  }
-
-  const removeItem = (index) => {
-    list.splice(index, 1)
-    setList([...list])
-  }
-
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(res => res.json())
+      .then(json => setData(json))
+  }, [])
+  
   return (
     <div>
-
-      <input onChange={(e) => setItem(e.target.value)} placeholder="Item" />
-      <button onClick={AddToList}>Add</button>
-
-      <table>
-        <tbody>
-          {
-            list.length !== 0 ? (
-              list.map((element, index) => {
-                return (
-                  <tr>
-                    <td>{element}</td>
-                    <td>
-                      <button
-                        onClick = {() => {
-                          removeItem(index)
-                        }}>
-                        Remove
-                      </button>
-                    </td>
-                  </tr>
-                )
-              })
-            )
-              :
-              (<tr>
-                <td>No Items</td>
-              </tr>)
-          }
-        </tbody>
-      </table>
+      {JSON.stringify(data)}
     </div>
   );
 };
